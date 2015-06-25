1e47776a18004e9d074d0f99bf258708de183313
Kevin Chappell <kevin.b.chappell@gmail.com>
2015-06-25 00:15:23 -0700

    demo WIP

--- README.md
+++ README.md
@@ -4,6 +4,3 @@
 A module for easy internationalization of AngularJS webapps.
 
 ## [Demo and Usage](http://kevinchappell.github.io/ng-i18n/)
-
-### Deprecated
-Angular 1.3+ has `i18n` built in. If your project however requires ie8 support then this is the module for you since you will be require to use Angular 1.2x anyways.
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 'use strict' ;
 
 var gulp    = require('gulp'),
+    gzip    = require('gulp-gzip'),
+    jshint  = require('gulp-jshint'),
     connect = require('gulp-connect'),
     karma   = require('gulp-karma'),
-    ugly   = require('gulp-uglify'),
+    ugly    = require('gulp-uglify'),
     concat  = require('gulp-concat');
 
+var srcFiles = [
+  'src/ng-i18n.js',
+  'src/ng-i18n-provider.js',
+  'src/ng-i18n-directive.js',
+  'src/ng-i18n-filter.js'
+  ];
+
 gulp.task('watch', function(){
-  gulp.watch(['src/**/*.js'], ['js']);
+  gulp.watch(['src/**/*.js'], ['lint', 'js']);
 });
 
 gulp.task('karma', function() {
@@ -18,18 +27,21 @@ gulp.task('karma', function() {
     }));
 });
 
+gulp.task('lint', function() {
+  return gulp.src(srcFiles)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
+});
+
 gulp.task('js', function () {
-  gulp.src([
-    'src/ng-i18n.js',
-    'src/ng-i18n-provider.js',
-    'src/ng-i18n-directive.js',
-    'src/ng-i18n-filter.js'
-    ])
+  gulp.src(srcFiles)
     .pipe(concat('ng-i18n.js'))
-    .pipe(gulp.dest('vendor/ng-i18n/'))
+    .pipe(gulp.dest('dist/'))
     .pipe(ugly())
     .pipe(concat('ng-i18n.min.js'))
-    .pipe(gulp.dest('vendor/ng-i18n/'));
+    .pipe(gzip())
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('serve', function() {
